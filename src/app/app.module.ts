3458e8a0979400b0b2db82c914d451da77fae05a
giovanasilveiraa <giovana.silveira01@etec.sp.gov.br>
2021-11-24 22:45:02 -0200

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProdutoComponent } from './components/produto/produto.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HomeComponent,
+    ProdutoComponent,
+    CarrinhoComponent,
+    PerfilComponent,
+    FavoritosComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
